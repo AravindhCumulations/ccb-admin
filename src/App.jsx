6e0504c5dbb46512d3179ded28676f214378018d
Aravindh <aravindh.s@cumulations.com>
2025-06-25 20:19:25 +0530

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SignInPage from './page/SignIn'
 import DashboardPage from './page/DashboardPage'
@@ -8,6 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<SignInPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
